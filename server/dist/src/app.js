6361394b69298dd4806646c97683ca9ad453e8a8
Jose Zambrana <eljosema505@gmail.com>
2024-12-12 11:48:13 +0100

    feat: Other way in the backend 3

--- server/dist/src/app.js
+++ server/dist/src/app.js
@@ -1,68 +1,4 @@
 "use strict";
-/* import Fastify from 'fastify';
-import cors from '@fastify/cors';
-import { categoryRoutes } from './app/categories/categories.controller';
-import { config } from './common/config/config';
-import { clubRoutes } from './app/clubs/clubs.controller';
-import { clubCategoriesRoutes } from './app/clubsCategories/clubs.controller';
-import { typeOfPassRoutes } from './app/typeOfPass/typeOfPass.controller';
-import { playerRoutes } from './app/player/player.controller';
-import { historyPlayerRoutes } from './app/historyPlayer/historyPlayer.controller';
-import { formatRoutes } from './app/format/format.controller';
-import { tournamentRoutes } from './app/tournaments/tournament.controller';
-import { gameRoutes } from './app/game/game.controller';
-import { usersRoutes } from './app/users/users.controller';
-
-let server:any;
-
-export function buildServer() {
-    if (!server) {
-        server = Fastify({ logger: true });
-
-        // Register plugins
-        server.register(cors, {
-            origin: '*',
-            methods: ['GET', 'POST', 'PUT', 'DELETE']
-        });
-
-        // Define routes
-        server.get('/', async (request:any, reply:any) => {
-            return reply.send({ service: 'welcome to soccer world' });
-        });
-
-        // Register application routes
-        usersRoutes(server);
-        categoryRoutes(server);
-        clubRoutes(server);
-        clubCategoriesRoutes(server);
-        typeOfPassRoutes(server);
-        playerRoutes(server);
-        historyPlayerRoutes(server);
-        formatRoutes(server);
-        tournamentRoutes(server);
-        gameRoutes(server);
-    }
-
-    return server;
-}
-
-if (require.main === module) {
-    // Run the server
-    const localServer = buildServer();
-
-    // Use Vercel's dynamic port if available
-    const port = process.env.PORT || config.port; // Default to config.port for local testing
-    const address = config.address;
-
-    localServer.listen({ port, host: address }, (err:any, address:string) => {
-        if (err) {
-            localServer.log.error(err);
-            process.exit(1);
-        }
-        console.log(`Server running at ${address}`);
-    });
-}
- */
 var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
     function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
     return new (P || (P = Promise))(function (resolve, reject) {
@@ -76,41 +12,58 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
     return (mod && mod.__esModule) ? mod : { "default": mod };
 };
 Object.defineProperty(exports, "__esModule", { value: true });
+exports.buildServer = buildServer;
 const fastify_1 = __importDefault(require("fastify"));
 const cors_1 = __importDefault(require("@fastify/cors"));
+const categories_controller_1 = require("./app/categories/categories.controller");
 const config_1 = require("./common/config/config");
-const server = (0, fastify_1.default)({ logger: true });
-const test = false;
-function main() {
-    return __awaiter(this, void 0, void 0, function* () {
+const clubs_controller_1 = require("./app/clubs/clubs.controller");
+const clubs_controller_2 = require("./app/clubsCategories/clubs.controller");
+const typeOfPass_controller_1 = require("./app/typeOfPass/typeOfPass.controller");
+const player_controller_1 = require("./app/player/player.controller");
+const historyPlayer_controller_1 = require("./app/historyPlayer/historyPlayer.controller");
+const format_controller_1 = require("./app/format/format.controller");
+const tournament_controller_1 = require("./app/tournaments/tournament.controller");
+const game_controller_1 = require("./app/game/game.controller");
+const users_controller_1 = require("./app/users/users.controller");
+let server;
+function buildServer() {
+    if (!server) {
+        server = (0, fastify_1.default)({ logger: true });
+        // Register plugins
         server.register(cors_1.default, {
             origin: '*',
             methods: ['GET', 'POST', 'PUT', 'DELETE']
         });
+        // Define routes
         server.get('/', (request, reply) => __awaiter(this, void 0, void 0, function* () {
-            return reply.send({ service: 'welcome to SIVI' });
+            return reply.send({ service: 'welcome to soccer world' });
         }));
-        /*     companyRoutes(server);
-            ComCategoriesRoues(server)
-            authRoutes(server)
-            companiesComCategoriesRoutes(server)
-            userRoutes(server)
-            commentRoutes(server)
-            prodCategoryRoutes(server)
-            productRoutes(server) */
-        if (test) {
-            return server;
-        }
-        try {
-            server.listen({ host: config_1.config.address, port: config_1.config.port }, (err) => { if (err)
-                throw err; });
-        }
-        catch (err) {
-            server.log.error(err);
+        // Register application routes
+        (0, users_controller_1.usersRoutes)(server);
+        (0, categories_controller_1.categoryRoutes)(server);
+        (0, clubs_controller_1.clubRoutes)(server);
+        (0, clubs_controller_2.clubCategoriesRoutes)(server);
+        (0, typeOfPass_controller_1.typeOfPassRoutes)(server);
+        (0, player_controller_1.playerRoutes)(server);
+        (0, historyPlayer_controller_1.historyPlayerRoutes)(server);
+        (0, format_controller_1.formatRoutes)(server);
+        (0, tournament_controller_1.tournamentRoutes)(server);
+        (0, game_controller_1.gameRoutes)(server);
+    }
+    return server;
+}
+if (require.main === module) {
+    // Run the server
+    const localServer = buildServer();
+    // Use Vercel's dynamic port if available
+    const port = process.env.PORT || config_1.config.port; // Default to config.port for local testing
+    const address = config_1.config.address;
+    localServer.listen({ port, host: address }, (err, address) => {
+        if (err) {
+            localServer.log.error(err);
             process.exit(1);
         }
+        console.log(`Server running at ${address}`);
     });
 }
-//if you wanna run the test, gotta comment the main()
-main();
-exports.default = main;
