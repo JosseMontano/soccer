894f2456ce6951fc63b93881ff019c2786ec3ef7
Jose Zambrana <eljosema505@gmail.com>
2024-12-12 12:19:48 +0100

    feat: Serverles in backend with the routes

--- server/dist/src/production.js
+++ server/dist/src/production.js
@@ -16,14 +16,30 @@ exports.default = handler;
 const fastify_1 = __importDefault(require("fastify"));
 const users_controller_1 = require("./app/users/users.controller");
 const categories_controller_1 = require("./app/categories/categories.controller");
+const clubs_controller_1 = require("./app/clubs/clubs.controller");
+const clubs_controller_2 = require("./app/clubsCategories/clubs.controller");
+const typeOfPass_controller_1 = require("./app/typeOfPass/typeOfPass.controller");
+const player_controller_1 = require("./app/player/player.controller");
+const historyPlayer_controller_1 = require("./app/historyPlayer/historyPlayer.controller");
+const format_controller_1 = require("./app/format/format.controller");
+const tournament_controller_1 = require("./app/tournaments/tournament.controller");
+const game_controller_1 = require("./app/game/game.controller");
 const app = (0, fastify_1.default)({
     logger: true,
 });
 app.get('/', (req, res) => __awaiter(void 0, void 0, void 0, function* () {
-    return res.send({ service: 'welcome to soccer world 1' });
+    return res.send({ service: 'welcome to soccer world' });
 }));
 (0, users_controller_1.usersRoutes)(app);
 (0, categories_controller_1.categoryRoutes)(app);
+(0, clubs_controller_1.clubRoutes)(app);
+(0, clubs_controller_2.clubCategoriesRoutes)(app);
+(0, typeOfPass_controller_1.typeOfPassRoutes)(app);
+(0, player_controller_1.playerRoutes)(app);
+(0, historyPlayer_controller_1.historyPlayerRoutes)(app);
+(0, format_controller_1.formatRoutes)(app);
+(0, tournament_controller_1.tournamentRoutes)(app);
+(0, game_controller_1.gameRoutes)(app);
 function handler(req, res) {
     return __awaiter(this, void 0, void 0, function* () {
         yield app.ready();
--- server/src/production.ts
+++ server/src/production.ts
@@ -1,17 +1,33 @@
 import Fastify from 'fastify'
 import { usersRoutes } from './app/users/users.controller';
 import { categoryRoutes } from './app/categories/categories.controller';
+import { clubRoutes } from './app/clubs/clubs.controller';
+import { clubCategoriesRoutes } from './app/clubsCategories/clubs.controller';
+import { typeOfPassRoutes } from './app/typeOfPass/typeOfPass.controller';
+import { playerRoutes } from './app/player/player.controller';
+import { historyPlayerRoutes } from './app/historyPlayer/historyPlayer.controller';
+import { formatRoutes } from './app/format/format.controller';
+import { tournamentRoutes } from './app/tournaments/tournament.controller';
+import { gameRoutes } from './app/game/game.controller';
 
 const app = Fastify({
   logger: true,
 })
 
 app.get('/', async (req, res) => {
-  return res.send({ service: 'welcome to soccer world 1' });
+  return res.send({ service: 'welcome to soccer world' });
 })
 
 usersRoutes(app);
 categoryRoutes(app);
+clubRoutes(app);
+clubCategoriesRoutes(app);
+typeOfPassRoutes(app);
+playerRoutes(app);
+historyPlayerRoutes(app);
+formatRoutes(app);
+tournamentRoutes(app);
+gameRoutes(app);
 
 export default async function handler(req: any, res: any) {
   await app.ready()
