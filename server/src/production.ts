d02f0164805fe3ddaf7090d7d7550c7b4aa98d6b
Jose Zambrana <eljosema505@gmail.com>
2024-12-12 12:03:01 +0100

    feat: Serverles in backend

---
+++ server/src/production.ts
@@ -0,0 +1,14 @@
+import Fastify from 'fastify'
+
+const app = Fastify({
+  logger: true,
+})
+
+app.get('/', async (req, res) => {
+  return res.send({ service: 'welcome to soccer world 1' });
+})
+
+export default async function handler(req: any, res: any) {
+  await app.ready()
+  app.server.emit('request', req, res)
+}
